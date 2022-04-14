bd8e516edf6c5f730ef53c9cc74bebb998a8f539
JoseC <josecaracas@gmail.com>
2022-04-13 23:02:11 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.new-cards',{delay:500});
+  ScrollReveal().reveal('.barner-one',{delay:500});
+  ScrollReveal().reveal('.barner-two',{delay:500});
+  ScrollReveal().reveal('.sociales',{delay:500});
